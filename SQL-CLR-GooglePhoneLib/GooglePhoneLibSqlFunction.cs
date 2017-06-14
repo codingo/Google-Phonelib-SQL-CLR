b5a15d4628e5836c3b1986f4e3f5a8b7da19de1e
Michael <codingo@outlook.com>
2017-06-14 12:03:51 +1000

    Test harness for IsValidNumber

--- SQL-CLR-GooglePhoneLib/GooglePhoneLibSqlFunction.cs
+++ SQL-CLR-GooglePhoneLib/GooglePhoneLibSqlFunction.cs
@@ -2,7 +2,10 @@ using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
+using com.google.i18n.phonenumbers;
+using libphonenumber;
 using Microsoft.SqlServer.Server;
+using PhoneNumberUtil = com.google.i18n.phonenumbers.PhoneNumberUtil;
 
 public partial class UserDefinedFunctions
 {
@@ -27,6 +30,14 @@ public partial class UserDefinedFunctions
     [Microsoft.SqlServer.Server.SqlFunction]
     public static SqlString GoogleIsValidNumber(string input)
     {
+
+        var phoneUtil = PhoneNumberUtil.getInstance();
+        bool isValid = phoneUtil.isValidNumber(swissNumberStr);
+
+        try
+        {
+            Phonenumber numberInput = phoneUtil.parse(input, "CH");
+        }
         return new SqlString(string.Empty);
     }
 
