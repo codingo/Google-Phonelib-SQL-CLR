7a31cc4ce58accb8c47fe355a38480ded63ed9c8
Michael <codingo@outlook.com>
2017-06-14 13:52:38 +1000

    Updating code comments

--- SQL-CLR-GooglePhoneLib/GooglePhoneLibSqlFunction.cs
+++ SQL-CLR-GooglePhoneLib/GooglePhoneLibSqlFunction.cs
@@ -9,27 +9,44 @@ using PhoneNumberUtil = com.google.i18n.phonenumbers.PhoneNumberUtil;
 
 public partial class UserDefinedFunctions
 {
-    // IsPossibleNumber - quickly guessing whether a number is a possible phonenumber by using only the length information, much faster than a full validation.
+    /// <summary>
+    /// IsPossibleNumber - quickly guessing whether a number is a possible phonenumber by using only the length information, much faster than a full validation.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
     [Microsoft.SqlServer.Server.SqlFunction]
     public static SqlString GoogleIsPossibleNumber(string input)
     {
         return new SqlString(string.Empty);
     }
 
-    // IsNumberMatch - gets a confidence level on whether two numbers could be the same.
+    /// <summary>
+    /// IsNumberMatch - gets a confidence level on whether two numbers could be the same.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
     [Microsoft.SqlServer.Server.SqlFunction]
     public static SqlString GoogleIsNumberMatch(string input)
     {
         return new SqlString(string.Empty);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
     [Microsoft.SqlServer.Server.SqlFunction]
     public static SqlString GoogleIsValidNumber(string input)
     {
         return new SqlString(string.Empty);
     }
 
-    // FindNumbers - finds numbers in text input
+    /// <summary>
+    /// FindNumbers - finds numbers in text input
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
     [Microsoft.SqlServer.Server.SqlFunction]
     public static SqlString GoogleFindNumbers(string input)
     {
