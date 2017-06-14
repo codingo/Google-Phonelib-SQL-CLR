262279c0fa8c3575dc76d7de956a74d75a3ddf49
Michael <codingo@outlook.com>
2017-06-14 13:47:56 +1000

    Added code commenting

--- SQL-CLR-GooglePhoneLib/GooglePhoneLibSqlFunction.cs
+++ SQL-CLR-GooglePhoneLib/GooglePhoneLibSqlFunction.cs
@@ -15,12 +15,14 @@ public partial class UserDefinedFunctions
         return new SqlString (string.Empty);
     }
 
+    // IsPossibleNumber - quickly guessing whether a number is a possible phonenumber by using only the length information, much faster than a full validation.
     [Microsoft.SqlServer.Server.SqlFunction]
     public static SqlString GoogleIsPossibleNumber(string input)
     {
         return new SqlString(string.Empty);
     }
 
+    // IsNumberMatch - gets a confidence level on whether two numbers could be the same.
     [Microsoft.SqlServer.Server.SqlFunction]
     public static SqlString GoogleIsNumberMatch(string input)
     {
